3a3ee546f3f3a1df2c61ce299a7a2fe7f66a1ade
Karolina Żurawska <karolina.zurawska88@gmail.com>
2022-12-19 15:33:02 +0100

    gdzie

---
+++ arithmeitc-operators.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main ()
+{
+    // arithmetic operators
+
+    // + (dodawanie)
+    // - (odejmowanie)
+    // * (mnożenie)
+    // / (dzielenie)
+    // % (modulus)
+    // ++ (increment)
+    // -- (decrement)
+
+    int x = 5;
+    int y = 2;
+    
+    y++;
+    
+    printf("%d ", y);
+
+    return 0;
+}
---
+++ variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 20;
+    char name[] = "Martyna";
+    char grade = 'A';
+    float gpa = 2.05;
+
+    printf("Your name is %s\n", name);
+    printf("You're %d years old\n", age);
+    printf("Your average grade is %c\n", grade);
+    printf("Your gpa is %f", gpa);
+
+    return 0;
+}
